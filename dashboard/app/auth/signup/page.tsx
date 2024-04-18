9407f1e033dd2a4f87be61e6c45f8e2c7702752d
banoni-dev <banoni.bro@gmail.com>
2024-04-18 13:54:24 +0100

    cleaning up the sign up

--- dashboard/app/auth/signup/page.tsx
+++ dashboard/app/auth/signup/page.tsx
@@ -18,20 +18,7 @@ const SignUp: React.FC = () => {
         <div className="flex flex-wrap items-center">
           <div className="hidden w-full xl:block xl:w-1/2">
             <div className="px-26 py-17.5 text-center">
-              <Link className="mb-5.5 inline-block" href="/">
-                <h1 className="text-3xl text-white">Shakib Admin</h1>
-                <Image
-                  className="dark:hidden"
-                  src={"/images/logo/logo-dark.svg"}
-                  alt="Logo"
-                  width={176}
-                  height={32}
-                />
-              </Link>
-              <p className="2xl:px-20">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                suspendisse.
-              </p>
+              
 
               <span className="mt-15 inline-block">
                 <svg
@@ -160,10 +147,6 @@ const SignUp: React.FC = () => {
 
           <div className="w-full border-stroke dark:border-strokedark xl:w-1/2 xl:border-l-2">
             <div className="w-full p-4 sm:p-12.5 xl:p-17.5">
-              <span className="mb-1.5 block font-medium">Start for free</span>
-              <h2 className="mb-9 text-2xl font-bold text-black dark:text-white sm:text-title-xl2">
-                Sign Up to ShakibAdmin
-              </h2>
 
               <form>
                 <div className="mb-4">

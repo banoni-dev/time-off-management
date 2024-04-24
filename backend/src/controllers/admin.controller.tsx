ba16cbfc64534a51600d2ab8948b66f7a5415156
banoni-dev <banoni.bro@gmail.com>
2024-04-24 23:58:43 +0100

    creating and modifing some functions in the admin controller (login,logout,create)

--- backend/src/controllers/admin.controller.tsx
+++ backend/src/controllers/admin.controller.tsx
@@ -1,9 +1,37 @@
 // import prisma client using require
+const jwt = require('jsonwebtoken');
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+const comparePasswords = (password: string, hashedPassword: string) => {
+  return password === hashedPassword;
+}
+
+const createAccessToken = (user: any) => {
+  return jwt.sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET!, {
+    expiresIn: '15m',
+  });
+};
+
+
+
 const login = async (req: Request, res: Response) => {
-  res.send("Hello from the admin controller!");
+  try {
+
+    const { username, password } = req.body;
+
+    const user = await prisma.user.findUnique({ where: { username } });
+
+    if (!user || !comparePasswords(password, user.password)) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    const accessToken = createAccessToken(user);
+    res.json({ accessToken });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 };
 
 const create = async (req: Request, res: Response) => {
@@ -19,7 +47,22 @@ const create = async (req: Request, res: Response) => {
   }
 };
 
+
+const logout = async (req: Request, res: Response) => {
+  try {
+    res.status(200).json({ message: "Logout successful" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+
+
+
+
 module.exports = {
   login,
   create,
+  logout
 };
